d1bdc5a130b04aacc079c70c1f7b6044ab14d287
Kakon Ray <thisiskakonray@gmail.com>
2022-07-20 23:56:37 +0600

    add product

---
+++ src/Component/Product/Product.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./Product.css";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ToggleButton from "react-bootstrap/ToggleButton";
+
+const Product = ({ title, description, img }) => {
+  return (
+    <div className="col-md-3">
+      <Card>
+        <Card.Img variant="top" src={img} />
+        <Card.Body className="text-center">
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+        <div className="btn-container">
+          <ButtonGroup>
+            <ToggleButton variant="secondary">Edit</ToggleButton>
+            <ToggleButton variant="danger">Delete</ToggleButton>
+          </ButtonGroup>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Product;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,43 @@
 import React from "react";
 import HomePageBanner from "../../Component/HomePageBanner/HomePageBanner";
-
+import Product from "../../Component/Product/Product";
+import "./Home.css";
+import product1 from "../../assets/img/product6.f19b14e6.png";
 const Home = () => {
   return (
-    <div>
+    <React.Fragment>
       <HomePageBanner />
-    </div>
+
+      <section className="product-section">
+        <div className="container">
+          <div className="row">
+            <h1 className="text-center product-main-title">Our Products</h1>
+          </div>
+          <div className="row">
+            <Product
+              title="Green Dress For Woman"
+              description="Green Dress For Woman Green Dress For Woman"
+              img={product1}
+            />
+            <Product
+              title="Green Dress For Woman"
+              description="Green Dress For Woman Green Dress For Woman"
+              img={product1}
+            />
+            <Product
+              title="Green Dress For Woman"
+              description="Green Dress For Woman Green Dress For Woman"
+              img={product1}
+            />
+            <Product
+              title="Green Dress For Woman"
+              description="Green Dress For Woman Green Dress For Woman"
+              img={product1}
+            />
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   );
 };
 
